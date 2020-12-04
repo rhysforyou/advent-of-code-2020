c21ee343b53b0bbb6e68b51cdd363ced9dc348ad
Rhys Powell <rhys@rpowell.me>
2020-12-04 11:18:19 +1100

    add part two solution

---
+++ 02-three-expenses/src/main.rs
@@ -0,0 +1,68 @@
+use std::{
+    env, fs,
+    io::{prelude::*, BufReader},
+};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let path = parse_args(&args);
+    let mut expenses = expenses_from_file(path);
+    expenses.sort_by(|a, b| a.cmp(b));
+    let (a, b, c) = three_expenses_with_sum(2020, &expenses)
+        .expect("Couldn't find expenses that summed to 2020");
+    let total = a * b * c;
+
+    println!("{}", total);
+}
+
+fn parse_args(args: &[String]) -> &str {
+    if args.len() < 2 {
+        panic!("Not enough arguments");
+    }
+    &args[1]
+}
+
+fn expenses_from_file(path: &str) -> Vec<i32> {
+    let file = fs::File::open(path).expect("Unable to read file");
+    let buf = BufReader::new(file);
+    buf.lines()
+        .map(|l| {
+            l.expect("Could not parse line")
+                .parse::<i32>()
+                .expect("Malformed input")
+        })
+        .collect()
+}
+
+fn two_expenses_with_sum(expected_sum: i32, expenses: &[i32]) -> Option<(i32, i32)> {
+    let mut start_index: usize = 0;
+    let mut end_index: usize = expenses.len() - 1;
+
+    while start_index != end_index {
+        let sum = expenses[start_index] + expenses[end_index];
+
+        if sum == expected_sum {
+            return Some((expenses[start_index], expenses[end_index]));
+        } else if sum < expected_sum {
+            start_index += 1;
+        } else {
+            end_index -= 1;
+        }
+    }
+
+    None
+}
+
+fn three_expenses_with_sum(expected_sum: i32, expenses: &[i32]) -> Option<(i32, i32, i32)> {
+    for (index, expense) in expenses.iter().enumerate() {
+        if index == expenses.len() - 2 {
+            return None;
+        }
+        match two_expenses_with_sum(expected_sum - expense, &expenses[index + 1..expenses.len()]) {
+            Some((a, b)) => return Some((*expense, a, b)),
+            None => continue,
+        }
+    }
+
+    None
+}
